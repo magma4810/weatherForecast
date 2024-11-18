7835a13c6b94c61fe4cab5624a42a0e1d06cef71
Pavel <magma4810@gmail.com>
2024-11-18 21:58:15 +0300

    add info block

--- src/init.js
+++ src/init.js
@@ -7,52 +7,29 @@ export async function init(div) {
 
   document.body.append(div);
   const inputCityHeader = document.createElement("header");
-  const inputCity = document.createElement("input");
-  inputCity.placeholder = "Enter the city";
-  inputCity.className = "inputCity";
 
-  const listCityes = document.createElement("ul");
-  listCityes.className = "listCityes";
-
-  const inputCityBlock = document.createElement("div");
-  inputCityBlock.className = "inputCityBlock";
-
-  const buttonInputCityHtml = document.createElement("button");
-  buttonInputCityHtml.textContent = "Enter";
-  buttonInputCityHtml.className = "buttonInputCity";
-  inputCityBlock.append(inputCity);
-  inputCityBlock.append(buttonInputCityHtml);
-
-  inputCityHeader.append(inputCityBlock);
-
-  inputCityHeader.append(listCityes);
+  inputCityHeader.innerHTML = `
+  <div class="inputCityBlock">
+  <input placeholder="Enter the city" class="inputCity">
+  <button class="buttonInputCity">Enter</button>
+  </div>
+  <ul class="listCityes">
+  </ul>`;
   div.append(inputCityHeader);
 
   const temperatureBlockWithInfo = document.createElement("div");
-  const temperatureBlockWithImg = document.createElement("div");
-  temperatureBlockWithImg.className = "temperatureBlockWithImg";
-
-  const temperature = document.createElement("div");
-  temperature.className = "temperature";
-
-  temperatureBlockWithImg.append(temperature);
-
-  const imgTemperature = document.createElement("img");
-  imgTemperature.className = "imgTemperature";
-
-  temperatureBlockWithImg.append(imgTemperature);
-  temperatureBlockWithInfo.append(temperatureBlockWithImg);
-
   temperatureBlockWithInfo.className = "temperatureBlock";
-  const weatherIn = document.createElement("div");
-  weatherIn.className = "weatherIn";
-  temperatureBlockWithInfo.append(weatherIn);
 
+  temperatureBlockWithInfo.innerHTML = `
+  <div class="temperatureBlockWithImg">
+  <div class="temperature"></div>
+  <img class="imgTemperature">
+    </div>
+    <div class="weatherIn"></div>`;
   div.append(temperatureBlockWithInfo);
 
   const informationBlock = document.createElement("div");
-  informationBlock.className = "information";
-  informationBlock.textContent = "information";
+  informationBlock.className = "informationBlock";
   div.append(informationBlock);
 
   if (!localStorage.getItem("cityes")) {
@@ -64,13 +41,12 @@ export async function init(div) {
     render(await userCity);
   } else {
     const lastCity = JSON.parse(localStorage.getItem("lastCity"));
-    //сделать обработку ошибок на непрвильные города
     render(lastCity);
   }
   let allCityes = JSON.parse(localStorage.getItem("cityes"));
   buttonInputCity();
-  inputCity.addEventListener("click", () => {
-    listCityes.classList.add("show");
+  document.querySelector(".inputCity").addEventListener("click", () => {
+    document.querySelector(".listCityes").classList.add("show");
   });
 
   allCityes.forEach((element) => {
--- src/render.js
+++ src/render.js
@@ -9,9 +9,14 @@ export async function render(city) {
     const weatherIn = document.querySelector(".weatherIn");
     const temperature = document.querySelector(".temperature");
     const imgTemperature = document.querySelector(".imgTemperature");
+    const information = document.querySelector(".informationBlock");
     weatherIn.textContent = `Погода в "${name}"`;
     temperature.textContent = `${weather.temp}˚`;
     imgTemperature.src = `https://openweathermap.org/img/wn/${img}@2x.png`;
+    information.innerHTML = `<div class="info">feels like : ${weather.feels_like}</div>
+    <div class="info">temp min : ${weather.temp_min}</div>
+    <div class="info">temp max : ${weather.temp_max}</div>
+    <div class="info">wind speed : ${response.wind.speed} km/h</div>`;
     return 1;
   } else {
     return null;
